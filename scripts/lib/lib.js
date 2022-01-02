68f4ee57debdfee963070e0a255f83945c400fb5
Timothy Coxon <timothy.coxon@gmail.com>
2022-01-02 14:26:20 +0000

    First Commit

---
+++ scripts/lib/lib.js
@@ -0,0 +1,9 @@
+/**
+ * @Author: Timothy Coxon <timcoxon>
+ * @Date:   02/01/22
+ * @Email:  timothy.coxon@gmail.com
+ * @Last modified by:   timcoxon
+ * @Last modified time: 02/01/22
+ * @License: http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @Copyright: Copyright (c) 2017 Quirkysites.co.uk
+ */
